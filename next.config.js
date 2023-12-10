edd910d172bb058dfa5f172dcc236aa82063cbd2
Ryan Lee <lee98064@gmail.com>
2023-12-10 18:09:01 +0800

    🔧 Add api proxy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 // @ts-check
 /** @type {import('next').NextConfig} */
 
+const { API_URL } = process.env;
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
